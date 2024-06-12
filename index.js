9c38eaf52b9343e73d75859ded2611e885fec95d
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-13 04:17:19 +0600

    user delete

--- index.js
+++ index.js
@@ -45,6 +45,11 @@ async function run() {
         const cartCollection = client.db("bistroDb").collection("carts");
 
         // users related api
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
         app.post('/users', async (req, res) => {
             const user = req.body;
             // insert if user does not exist
@@ -63,9 +68,16 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = res.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+
+        })
 
 
-
+        // menu related api
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
