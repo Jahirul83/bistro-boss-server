88348c6420abeb3c61d553abfbea13ec9a8fe47a
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-13 04:53:42 +0600

    jwt install

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
